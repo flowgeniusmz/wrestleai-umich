9e208777fd8b93ed9fc05218eb7a9e29d8a056d0
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2024-05-03 08:10:58 +0000

    added pagesetup to all pages

---
+++ pages/9_Rule_Change_Updates_📢.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from config import pagesetup as ps, sessionstates as ss
+
+
+# 0. Set page config
+st.set_page_config(page_title=st.secrets.appconfig.app_name, page_icon=st.secrets.appconfig.app_icon, layout=st.secrets.appconfig.app_layout, initial_sidebar_state=st.secrets.appconfig.app_initial_sidebar)
+page = 8
+ps.master_page_display_styled_popmenu_pop(varPageNumber=page)
