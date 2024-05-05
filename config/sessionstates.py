9575d33197f63a2802a0bf1fce44684b541c9270
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2024-05-05 09:14:05 +0000

    j

--- config/sessionstates.py
+++ config/sessionstates.py
@@ -3,4 +3,4 @@ import streamlit as st
 def initialize_session_states():
     if "chat_messages" not in st.session_state:
         st.session_state.chat_messages = [{"role": "assistant", "content": "Welcome to WrestleAI! How can I assist you?"}]
-        st.session_state.image_paths = ["images/header.png", "images/header1.png", "images/header2.png", "images/header4.png"]
+        st.session_state.image_paths = ["images/2f3aaec0065b11e28b0122000a1e9b8c_7.png", "images/153398ec3601a31fe8b61761d6a43f26_1024x1024 (1).png", "images/wsi-imageoptim-highcrotchtakedownfeatureimage.png"]
